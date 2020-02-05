02fa5bb3e9127a076207c9d12ca1c428d48cd85d
Miroslav Rutesic <miroslav.rutesic@bild-studio.net>
2020-02-05 14:45:04 +0100

    Change placement of functions/ clear whitespaces

--- app.js
+++ app.js
@@ -2,15 +2,17 @@ let uniqueID = 0;
 let myLibrary = [{title: "The Willows", author: "Algernon Blackwood" },
                  {title: "The Wine-Dark Sea", author: "Robert Aickman"}];
 
-let addButton = document.getElementById('add-btn');
-addButton.addEventListener('click', createBook, false);
-addButton.addEventListener('click', (event) => { event.preventDefault() } )
-addButton.addEventListener('click', toggle, false);
 
-function toggle() {
-  let modal = document.querySelector('.w3-modal');
-  modal.style.display = 'none';
+
+class Book {
+  constructor(title, author) {
+    this.id = ++uniqueID;
+    this.title  = title;
+    this.author = author;
+  }
 }
+
+
 
 function createBook() {
   book = getFormValues();
@@ -22,14 +24,6 @@ function getFormValues() {
   let title = document.getElementById('title').value;
   let author = document.getElementById('author').value;
   return {title, author};
-}
-
-class Book {
-  constructor(title, author) {
-    this.id = ++uniqueID;
-    this.title  = title;
-    this.author = author;
-  }
 }
 
 function addBookToLibrary(book) {
@@ -88,5 +82,14 @@ function makeBookButtons() {
   return buttons;
 }
 
+let addButton = document.getElementById('add-btn');
+addButton.addEventListener('click', createBook, false);
+addButton.addEventListener('click', (event) => { event.preventDefault() } )
+addButton.addEventListener('click', toggle, false);
+
+function toggle() {
+  let modal = document.querySelector('.w3-modal');
+  modal.style.display = 'none';
+}
 
 render(myLibrary);
